84023a6bed0f97c5a119ab8dc9e8380ccfe4fbbb
Daniel Maslowski <info@orangecms.org>
2021-06-27 13:43:21 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import * as webpack from "webpack";
+import { getOptions } from "loader-utils";
 import {readFileSync, unlinkSync} from "fs";
 import {basename, join} from "path";
 import {execFile} from "child_process";
 
-const proxyBuilder = (filename: string) => `
+export const proxyBuilder = (filename: string) => `
 export default gobridge(fetch('${filename}').then(response => response.arrayBuffer()));
 `;
 
 const getGoBin = (root: string) => `${root}/bin/go`;
 
 function loader(this: webpack.loader.LoaderContext, contents: string) {
+  const options = getOptions(this);
+  console.info({ options });
   const cb = this.async();
 
   let resourceDirectory = this.resourcePath.substr(0, this.resourcePath.lastIndexOf("/"));
@@ -39,7 +42,11 @@ function loader(this: webpack.loader.LoaderContext, contents: string) {
     console.info({ goBin, args, opts, err });
 
     const out = readFileSync(outFile); // was `let`; necessary?
-    unlinkSync(outFile);
+    try {
+      unlinkSync(outFile);
+    } catch (e) {
+      console.info('unlinking encountered error:', { e });
+    }
 
     const emitFileBasename = basename(this.resourcePath, ".go");
     const emittedFilename = `${emitFileBasename}.wasm`;
@@ -50,10 +57,12 @@ function loader(this: webpack.loader.LoaderContext, contents: string) {
     const bridgePath = join(__dirname, "..", "dist", "gobridge.js");
     // FIXME: This `fetch` wants an absolute address!
     // `__webpack_public_path__ + ${emittedFilename}`?
+    // console.info(__webpack_public_path__);
     const proxied = `
       require('!${libPath}');
       import gobridge from '${bridgePath}';
-      const file = fetch('${emittedFilename}');
+      console.info({ wpPubPath:__webpack_public_path__ }, window.navigator);
+      const file = fetch(__webpack_public_path__ + '${emittedFilename}');
       const buffer = file.then(res => res.arrayBuffer());
       export default gobridge(buffer);
     `;
